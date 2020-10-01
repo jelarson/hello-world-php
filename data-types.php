dcc7c5955b0f216cf0cec0df6cbcfb3f5747b805
jelarson <jesslarson93@gmail.com>
2020-10-01 09:42:01 -0600

    intro to some data types in php

---
+++ data-types.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php
+//string
+  $phrase = "To be or not to be";
+  echo $phrase
+//number
+  $age = 30;
+  echo $age
+//floating point or decimal point number
+  $gpa = 3.34;
+  echo $gpa
+//boolean
+  $isMale = true;
+  echo $isMale
+//null
+  null;
+?>
+  
+</body>
+</html>
