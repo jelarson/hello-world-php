026d6060b64b06ab9e77302dae301086a03c42aa
jelarson <jesslarson93@gmail.com>
2020-10-02 10:45:17 -0600

    intro to php functions

---
+++ functions.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+  <meta charset='UTF-8'>
+  <title></title>
+</head>
+<body>
+  
+<?php
+  function sayHi($name, $age){
+    echo "Hello $name, you are $age years old <br>";
+  }
+
+  sayHi("Jesse", 27);
+  sayHi("Tom", 13);
+  sayHi("Dave", 80);
+?>
+
+</body>
+</html>
