2e8f130734bdc1421a377fb50ce085f5732b7f32
jelarson <jesslarson93@gmail.com>
2020-10-05 14:28:44 -0600

    While and do/while loops intro in php

---
+++ while-loop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+  <meta charset='UTF-8'>
+  <title></title>
+</head>
+<body>
+  
+<?php
+  // $index = 1;
+  // while($index <= 5){
+  //   echo "$index <br>";
+  //   $index++;
+  // }
+  $index = 6;
+  do{
+  echo "$index <br>";
+  $index++;
+}while($index <= 5);
+// do while runs once and then checks condition
+?>
+
+</body>
+</html>
